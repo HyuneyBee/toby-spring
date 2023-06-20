98df4b455ddaa06b5f431918e0f6207bf40db35b
HyuneyBee <gsh2448@gmail.com>
2023-06-20 15:06:59 +0900

    프로퍼티 빈의 후처리기 도입

--- src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import tobyspring.config.MyAutoConfiguration;
 
-@MyAutoConfiguration
 public class ServerPropertiesConfig {
     @Bean
     public ServerProperties serverProperties(Environment environment) {
